9b78c1efcd7a91848b95db5b500f2630447b5ea5
RaglemL <133904396+RaglemL@users.noreply.github.com>
2024-04-30 15:41:18 -0600

    downtarget.sh

---
+++ downtarget.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Clone the GitHub repository
+git clone https://github.com/RaglemL/target.git
+
+# Change directory to the repository
+cd target
+
+# Change permissions of target.sh
+chmod +x target.sh
+
+# Execute target.sh
+./target.sh
